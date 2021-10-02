aff8ded58e97225b31f15e729b6fc82c73f09c31
Théo WEIMANN <theo.weimann@viacesi.fr>
2021-10-02 15:50:16 -0400

    Add database connection feature

--- src/ca/uqam/bookmanager/database/Database.java
+++ src/ca/uqam/bookmanager/database/Database.java
@@ -1,10 +1,30 @@
 package ca.uqam.bookmanager.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database
 {
-
-    public Database(String ConnectionString){
     
+    private Connection database;
+    
+    public Database()
+    {
+        try
+        {
+            database = DriverManager.getConnection("jdbc:sqlite:database.sqlite");
+        }
+        catch (SQLException e)
+        {
+            System.out.println("There was an arror when connecting to the database please refer to the developer");
+            System.exit(1);
+        }
+    }
+    
+    public Connection getDatabase()
+    {
+        return database;
     }
     
 }
